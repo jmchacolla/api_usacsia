7341a7d94d916d3274bf38b878b8ee9116f6b50f
vero <vronk_tcviral@hotmail.com>
2017-12-09 15:49:22 -0400

    controller persona tramite

--- app/Http/Controllers/Persona_tramiteController.php
+++ app/Http/Controllers/Persona_tramiteController.php
@@ -1,35 +1,31 @@
 <?php
 
 namespace App\Http\Controllers;
-use Carbon;
 
 use Illuminate\Http\Request;
 use Validator;
 use App\Http\Requests;
-use App\Models\Persona_tramite;
-use App\Models\Persona;
-use App\Models\Tramite;
+use App\Models\Persona_Tramite;
+use App\Models\Muestra;
+
+
 
 
 class Persona_tramiteController extends Controller
-
 {
-    /*public function index()
-    {
-        # code...
-        print(now());
-    }*/
     public function listar_x_tipo_tramite($tra_id)
     {
         /*  
             1: lista de tramites de carnet sanitario
             2: lista de tramites de certificado sanitario
         */
-        $pers_tramite=Persona_tramite::select('tramite.tra_nombre', 'persona.per_id','persona.per_ci','persona.per_nombres','persona.per_apellido_primero','persona.per_apellido_segundo','persona.per_fecha_nacimiento', 'persona.per_genero','persona.per_ocupacion','pt_tipo_tramite')
+        $pers_tramite=Persona_Tramite::select('tramite.tra_nombre', 'persona.per_id','persona.per_ci','persona.per_nombres','persona.per_apellido_primero','persona.per_apellido_segundo','persona.per_fecha_nacimiento', 'persona.per_genero','persona.per_ocupacion','pt_tipo_tramite')
         ->join('tramite','tramite.tra_id','=','persona_tramite.tra_id')
         ->join('persona', 'persona.per_id', '=', 'persona_tramite.per_id')
         ->where('persona_tramite.tra_id', $tra_id)
         ->get();
+
+
 
         return response()->json(['status'=>'ok','mensaje'=>'exito','persona_tramite'=>$pers_tramite],200);
     }
@@ -46,17 +42,17 @@ class Persona_tramiteController extends Controller
         {
             return $validator->errors()->all();
 		}  */
-		$persona_tramite= new Persona_tramite();
-		
+		$persona_tramite= new \App\Models\Persona_Tramite();
+		$persona_tramite->tra_id=$request->tra_id;
 		$persona_tramite->per_id=$request->per_id;
-		// $persona_tramite->pt_fecha_fin=$request->pt_fecha_fin;
-		// $persona_tramite->pt_estado_tramite=$request->pt_estado_tramite;
-        $persona_tramite->pt_vigencia_pago=$request->pt_vigencia_pago   ;//-------//desde base de datos
-        $persona_tramite->pt_monto=$request->pt_monto;
-        $persona_tramite->pt_tipo_tramite=$request->pt_tipo_tramite;
-        // $persona_tramite->pt_numero_tramite = $request->pt_numero_tramite; //serial
-        // $persona_tramite->pt_fecha_ini=now();currente date base de datos
-        // $persona_tramite->pt_estado_pago=$request->pt_estado_pago;//por defecto pagado en base de datos
+		$persona_tramite->pt_numero_tramite = $request->pt_numero_tramite;
+		$persona_tramite->pt_vigencia_pago=$request->pt_vigencia_pago;
+		$persona_tramite->pt_fecha_ini=$request->pt_fecha_ini;
+		$persona_tramite->pt_fecha_fin=$request->pt_fecha_fin;
+		$persona_tramite->pt_estado_pago=$request->pt_estado_pago;
+		$persona_tramite->pt_estado_tramite=$request->pt_estado_tramite;
+		$persona_tramite->pt_monto=$request->pt_monto;
+		$persona_tramite->pt_tipo_tramite=$request->pt_tipo_tramite;
 
 		$persona_tramite->save();
 
@@ -77,16 +73,16 @@ class Persona_tramiteController extends Controller
        // $amb_id=$ambiente->amb_id;
         //$ambientes= \App\Models\Ambiente::find($amb_id);
         
-       	// $persona_tramite->tra_id=$request->tra_id;
-		// $persona_tramite->per_id=$request->per_id;no se modifica
-		// $persona_tramite->pt_numero_tramite = $request->pt_numero_tramite; no se modifica
+       	$persona_tramite->tra_id=$request->tra_id;
+		$persona_tramite->per_id=$request->per_id;
+		$persona_tramite->pt_numero_tramite = $request->pt_numero_tramite;
 		$persona_tramite->pt_vigencia_pago=$request->pt_vigencia_pago;
-		// $persona_tramite->pt_fecha_ini=$request->pt_fecha_ini;//no se modifica
+		$persona_tramite->pt_fecha_ini=$request->pt_fecha_ini;
 		$persona_tramite->pt_fecha_fin=$request->pt_fecha_fin;
 		$persona_tramite->pt_estado_pago=$request->pt_estado_pago;
 		$persona_tramite->pt_estado_tramite=$request->pt_estado_tramite;
 		$persona_tramite->pt_monto=$request->pt_monto;
-		// $persona_tramite->pt_tipo_tramite=$request->pt_tipo_tramite;
+		$persona_tramite->pt_tipo_tramite=$request->pt_tipo_tramite;
        /* $ambientes->userid_at='2';*/
         $persona_tramite->save();
 
@@ -98,17 +94,15 @@ class Persona_tramiteController extends Controller
 
      public function show($pt_id)
     {
-        $persona_tramite= Persona_tramite::find($pt_id);
+        $persona_tramite= \App\Models\Persona_Tramite::find($pt_id);
         if (!$persona_tramite)
         {
 
             return response()->json(['errors'=>array(['code'=>404,'message'=>'No se encuentra la persona_tramite con ese código.'])],404);
         }
-
-        $persona=Persona::find($persona_tramite->per_id);
-        $tramite=Tramite::find($persona_tramite->tra_id);
-        $resultado=compact('persona_tramite', 'persona', 'tramite');
-        return response()->json(['status'=>'ok','persona_tramite'=>$resultado],200);
+  
+       
+        return response()->json(['status'=>'ok','persona_tramite'=>$persona_tramite],200);
     }
      public function destroy($pt_id)
     {
@@ -129,7 +123,7 @@ class Persona_tramiteController extends Controller
         );
     }
 
-     public function buscar_persona_tramite($per_ci)//por CI
+     public function buscar_persona_tramite($per_ci)
     {
         $hoy=date('Y-m-d');
         $ultima_muestra=Muestra::select('muestra.mue_num_muestra')
@@ -140,7 +134,7 @@ class Persona_tramiteController extends Controller
         if(!$ultima_muestra)
         {
             $numero_muestra=1;
-        }
+        } 
 
         $persona_tramite = Persona_Tramite::select('per_nombres','per_apellido_primero', 'per_apellido_segundo', 'per_ci', 'per_ci_expedido','mue_num_muestra')
         ->join('persona', 'persona.per_id','=', 'persona_tramite.per_id')
@@ -150,7 +144,7 @@ class Persona_tramiteController extends Controller
         if (!$persona_tramite->first())
         {    
             return response()->json(["mensaje"=>"no se encuentra una persona_tramite con ese codigo"]);
-        }
+        } 
          return response()->json(['status'=>'ok','mensaje'=>'exito',"persona_tramite"=>$persona_tramite, "numero_muestra"=>$numero_muestra], 200);
     }
 
